5e5b2ebe05f9abbb2f59ea7ba15d3d33a09336e2
kennyjung0223 <55126459+kennyjung0223@users.noreply.github.com>
2020-09-23 00:40:38 -0700

    Add files via upload

--- kjung10.cpp
+++ kjung10.cpp
@@ -1,31 +1,54 @@
 // I used the textbook and lecture notes to complete this lab assignment.
-
 #include <iostream>
+#include<climits>
 using namespace std;
 
-int linear_search(int arr[], int size, int key) {
-    // linear search is a very intuitive searching algorithm that takes in the list of elements and a key
-    // in this implementation, we start from left to right, traversing through each element in the list
-    // at each element, we compare its value with the key, if there is a match, we return that index, otherwise, we keep searching
-    // if we reach the end of the list and did find any match, we return -1 as representative of "the key is not in the list"
-    for (int i = 0; i < size; i++) {
-        // iterate through the entire array
-        if (arr[i] == key) {
-            // make comparison with element and key, return the index if there is a match
-            return i;
+int max_cross_subarray(int arr[], int low, int mid, int high) {
+    int left_sum = INT_MIN;
+    int sum = 0;
+
+    for (int i = mid; i >= low; i--) {
+        sum += arr[i];
+
+        if (sum > left_sum) {
+            left_sum = sum;
         }
     }
-    // return -1 when key is not in the array
-    return -1;
+
+    int right_sum = INT_MIN;
+    sum = 0;
+
+    for (int j = mid + 1; j <= high; j++) {
+        sum += arr[j];
+
+        if (sum > right_sum) {
+            right_sum = sum;
+        }
+    }
+
+    return left_sum + right_sum;
+}
+
+int max_subarray(int arr[], int low, int high) {
+
+    if (low == high) {
+        return arr[low];
+    }
+    
+    int mid = low + (high - low) / 2;
+
+    int left_sum = max_subarray(arr, low, mid);
+    int right_sum = max_subarray(arr, mid + 1, high);
+    int cross_sum = max_cross_subarray(arr, low, mid, high);
+
+    int max_part = max(left_sum, right_sum);
+    return max(max_part, cross_sum);
 }
 
 int main() {
 
     int size;
-    int key;
-
     cin >> size;
-    cin >> key;
 
     int arr[size];
 
@@ -33,9 +56,8 @@ int main() {
         cin >> arr[i];
     }
 
-    int index = linear_search(arr, size, key);
-
-    cout << index << endl;
+    int max_subarray_sum = max_subarray(arr, 0, size - 1);
+    cout << max_subarray_sum;
 
     return 0;
 }
