152379fc18a3f7e697ce366f27c1a8f78088e937
kennyjung0223 <55126459+kennyjung0223@users.noreply.github.com>
2020-09-27 23:19:30 -0700

    Add files via upload

--- kjung10.cpp
+++ kjung10.cpp
@@ -1,48 +1,60 @@
-// I used the textbook and lecture notes to complete this lab assignment.
 #include <iostream>
-#include<climits>
 using namespace std;
 
-int max_cross_subarray(int arr[], int low, int mid, int high) {
-    int left_sum = INT_MIN;
-    int sum = 0;
-
-    for (int i = mid; i >= low; i--) {
-        sum += arr[i];
-
-        if (sum > left_sum) {
-            left_sum = sum;
-        }
-    }
-
-    int right_sum = INT_MIN;
-    sum = 0;
-
-    for (int j = mid + 1; j <= high; j++) {
-        sum += arr[j];
-
-        if (sum > right_sum) {
-            right_sum = sum;
-        }
-    }
-
-    return left_sum + right_sum;
+// helper functions
+int get_parent(int index) {
+    return (index - 1) / 2;
 }
 
-int max_subarray(int arr[], int low, int high) {
+int get_left_child(int index) {
+    return 2 * index + 1;
+}
 
-    if (low == high) {
-        return arr[low];
+int get_right_child(int index) {
+    return 2 * index + 2;
+}
+
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// max-heapify
+void max_heapify(int arr[], int index, int heap_size) {
+    int left = get_left_child(index);
+    int right = get_right_child(index);
+    int largest = index;
+
+    if (left <= heap_size && arr[left] > arr[largest]) {
+        largest = left;
     }
-    
-    int mid = low + (high - low) / 2;
+    if (right <= heap_size && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest != index) {
+        swap(arr, index, largest);
+        max_heapify(arr, largest, heap_size);
+    }
+}
 
-    int left_sum = max_subarray(arr, low, mid);
-    int right_sum = max_subarray(arr, mid + 1, high);
-    int cross_sum = max_cross_subarray(arr, low, mid, high);
+// build-max-heap
+void build_max_heap(int arr[], int heap_size) {
+    int parent = get_parent(heap_size);
 
-    int max_part = max(left_sum, right_sum);
-    return max(max_part, cross_sum);
+    for (int i = parent; i >= 0; i--) {
+        max_heapify(arr, i, heap_size);
+    }
+}
+
+// heap-sort
+void heap_sort(int arr[], int heap_size) {
+    build_max_heap(arr, heap_size - 1);
+
+    for (int i = heap_size - 1; i > 0; i--) {
+        swap(arr, 0, i);
+        max_heapify(arr, 0, i - 1);
+    }
 }
 
 int main() {
@@ -51,13 +63,16 @@ int main() {
     cin >> size;
 
     int arr[size];
-
+    
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
 
-    int max_subarray_sum = max_subarray(arr, 0, size - 1);
-    cout << max_subarray_sum;
+    heap_sort(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << ';';
+    }
 
     return 0;
 }
